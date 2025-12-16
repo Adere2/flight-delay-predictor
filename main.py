267db0f31b7772ba5013faf40aeb279e773df742
adere <62905304+Adere2@users.noreply.github.com>
2025-12-16 10:17:26 -0500

    create main script

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from flight-delay-predictor!")
+import sys
+import warnings
 
+from src.predict import FlightOracle
+from src.train import train_pipeline
+
+warnings.filterwarnings("ignore", category=UserWarning)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print("Usage: python main.py [train|predict]")
+        sys.exit(1)
+
+    command = sys.argv[1]
+
+    if command == "train":
+        # Point this to your raw CSV
+        train_pipeline("data/raw/", "models/")
+
+    elif command == "predict":
+        oracle = FlightOracle("models/")
+        # Example prediction
+        mins, prob = oracle.predict("AA", "ABQ", "DFW", "2025-12-30", "19:00", 569)
+        print(f"Predicted Delay: {mins:.1f} mins")
+        print(f"Probability of Late: {prob * 100:.1f}%")
+
+    else:
+        print("Unknown command.")
